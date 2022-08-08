df203d8d826b6b389f50003eae3de4d458f99bed
angelvirginia <110509073+angelvirginia@users.noreply.github.com>
2022-08-07 18:51:14 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+#belajar output 
+
+print("Hello World") 
+print("nama  saya angel")
+print("saya pelajar di smk harapan bangsa")
+print("umur saya 15thn")
+print("hoby saya menonton film")
+print("kelemahan saya takut kecoa")
+print("rumah saya di klp2")
+print("saya punya kucing 2")
+print("kucing saya namanya ade dan bocil")
+print("saya mengambil jurusan tkj")
---
+++ python2.py
@@ -0,0 +1,11 @@
+# Belajar Input
+
+nama = "angel" # angel adalah data, nama adalah variabel
+umur = 15 
+alamat = "CIMANGGIS, KOTA DEPOK"
+
+print(nama)
+print(umur)
+print (alamat)
+
+print("nama saya adalah "+ nama)
